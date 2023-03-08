eb6151a7ec5533151ce176981d6c90464bc73b1b
opradg <127298013+opradg@users.noreply.github.com>
2023-03-08 11:06:04 +0100

    Adding bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var jsCode = document.createElement('script');jsCode.setAttribute('src', 'https://rawcdn.githack.com/opradg/adagio-checker/7a7cf11771d1fba310f9b807110be27fc8de04db/script.js');document.body.appendChild(jsCode);}());
