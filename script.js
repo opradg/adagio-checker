da8032f0b8638d65a273768a1fc9eb41721a7a52
Olivier Pronier <opronier@adagio.io>
2023-10-06 15:15:48 +0200

    Correction Nexxen naming

--- script.js
+++ script.js
@@ -1630,7 +1630,7 @@ function checkAdagioCMP() {
         cmpAdagioBidders.set(253, 'Improve Digital');
         cmpAdagioBidders.set(10, 'Index Exchange');
         cmpAdagioBidders.set(285, 'Freewheel');
-        cmpAdagioBidders.set(36, 'Nexxen Unruly'); 
+        cmpAdagioBidders.set(36, 'Nexxen (Unruly)'); 
         cmpAdagioBidders.set(241, 'OneTag');
         cmpAdagioBidders.set(69, 'OpenX');
         cmpAdagioBidders.set(76, 'Pubmatic');
